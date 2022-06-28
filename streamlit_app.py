ae935e2ea3d06789d13aec1b5519415d473768c5
VinnyRazdan007 <106469768+VinnyRazdan007@users.noreply.github.com>
2022-06-28 21:14:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_ch
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment - It creates the table 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - It displays the data in tables
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 #streamlit.dataframe(my_fruit_list)
 #Let's put a pick list here so they can pick the fruit they want to include
