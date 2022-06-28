92e9bb56a403a372b63a5350a5e3d940963dbdb4
VinnyRazdan007 <106469768+VinnyRazdan007@users.noreply.github.com>
2022-06-28 11:35:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,13 +23,22 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-# write your own comment -what does the next line do? 
+#take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+#output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response.json())
+
+# write your own comment - It creates the table 
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - It displays the data in tables
+#streamlit.dataframe(fruityvice_normalized)
 
 #streamlit.dataframe(my_fruit_list)
 #Let's put a pick list here so they can pick the fruit they want to include
