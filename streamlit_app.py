470f3ca6151b58d341b0f06ac276c1d14de47c8e
VinnyRazdan007 <106469768+VinnyRazdan007@users.noreply.github.com>
2022-06-30 11:47:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,13 +23,28 @@ streamlit.dataframe(fruits_to_show)
 #streamlit.text(fruityvice_response)
 
 #New Section to display fruityvice api response
+#streamlit.header('Fruityvice Fruit Advice!')
+#fruit_choice = streamlit.text_input('What fruit would you like information about ?', )
+#streamlit.write('The user entered', fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
+
+#New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about ?', )
-streamlit.write('The user entered', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
+try:
+	fruit_choice = streamlit.text_input('What fruit would you like information about ?')
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")
+	else:
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+	streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+	streamlit.error()
+  
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json version of the response and normalize it
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -42,9 +57,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment - It creates the table 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - It displays the data in tables
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 #streamlit.dataframe(my_fruit_list)
 #Let's put a pick list here so they can pick the fruit they want to include
